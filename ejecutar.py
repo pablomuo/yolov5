99404e98d7e2785a47641802e6fdf558b4a0df41
pablomuo <villarpablo99@gmail.com>
2022-10-19 09:25:51 +0200

    v8

--- ejecutar.py
+++ ejecutar.py
@@ -6,6 +6,6 @@ import time
 os.system('python servidor_broadcast.py &')
 time.sleep(3)
 
-os.system('python detect_3.py --source 0 --weights best.pt')
+os.system('python detect.py --source 0 --weights best.pt')
 #os.system('python detect1.py --weights Detection/exp4/weights/best.pt > data.txt')
 #os.system('ping 10.236.52.82 | tee data.txt')
